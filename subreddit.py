5d76826c7bdbaec251b2ec8ff224f87548238c4d
Harsh <harsh.pandey1357@gmail.com>
2020-07-16 15:00:48 +0530

    Update subreddit.py
    changed output file from .txt to .csv

--- subreddit.py
+++ subreddit.py
@@ -19,10 +19,10 @@ def deEmojify(text):
                            "]+", flags = re.UNICODE)
     return regrex_pattern.sub(r'',text)
 # create a file to store the parsed comments
-if os.path.isfile('output.txt'):
-    f = open("output.txt", "a+", encoding = 'utf-8')
+if os.path.isfile('output.csv'):
+    f = open("output.csv", "a+", encoding = 'utf-8')
 else:
-    f = open("output.txt", "w+", encoding = 'utf-8') 
+    f = open("output.csv", "w+", encoding = 'utf-8') 
 # giving in credentials for accessing the reddit data   
 # refer the readme to access your credentials
 # register though the given link and then fill in your credentials
